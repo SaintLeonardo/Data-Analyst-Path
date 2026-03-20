aca4024a82036c39852bb766a604c7dbf863e8b3
Leonardo <leonardo.ls@gec.inatel.br>
2026-03-20 14:49:36 -0300

    Fixing and Learning more SQL
    At this moment I'm trainning my SQL skills, but I'm still learning and fixing some concepts with exercises.

---
+++ SQL/Hacker Rank questions.sql
@@ -0,0 +1,71 @@
+-- Hacker Rank questions
+-- EASY DIFFICULT
+-- Revising the Select Query I
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+SELECT * FROM City WHERE Population > 100000 AND Countrycode = "USA";
+
+-- Revising the Select Query II
+SELECT Name FROM City
+WHERE Population > 120000 AND Countrycode = "USA";
+
+-- Query all columns (attributes) for every row in the CITY table.
+SELECT * FROM CITY;
+
+-- Select By ID
+SELECT * FROM CITY
+WHERE ID = 1661;
+
+-- Japanese Cities' Attributes
+SELECT * FROM City
+WHERE Countrycode = "JPN";
+
+-- Japanese Cities' Names
+SELECT Name FROM City
+WHERE Countrycode = "JPN";
+
+-- Weather Observation Station 1
+SELECT City, State 
+FROM Station;
+
+-- Weather Observation Station 3
+SELECT DISTINCT City 
+FROM STATION
+WHERE (ID%2) = 0;
+
+-- Weather Observation Station 4
+SELECT COUNT(City) - COUNT(DISTINCT City)
+FROM Station;
+
+-- Weather Observation Station 5
+SELECT City, LENGTH(city) AS MIN_Value
+FROM Station
+ORDER BY LENGTH(city) ASC, city ASC
+LIMIT 1;
+
+SELECT City, LENGTH(city) AS MAX_Value
+FROM Station
+ORDER BY LENGTH(city) DESC, city DESC
+LIMIT 1;
+
+-- Weather Observation Station 6
+SELECT DISTINCT city
+FROM Station
+WHERE city LIKE "a%" OR  city LIKE "e%" OR city LIKE "i%" OR city LIKE "o%" OR city LIKE "u%"
+
+-- Weather Observation Station 7
+SELECT DISTINCT city
+FROM Station
+WHERE city LIKE "%a" OR city LIKE "%e" OR city LIKE "%i" OR city LIKE "%o" OR city LIKE "%u";
+
+-- Weather Observation Station 8
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) IN ('A','E','I','O','U')
+  AND RIGHT(CITY,1) IN ('A','E','I','O','U');
+
+-- Weather Observation Station 9
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LEFT(CITY,1) NOT IN ('A','E','I','O','U');
+
+-- Weather Observation Station 10
