d291d3ad97cece5a324c668f0e087a07a298a601
Leonardo <leonardo.ls@gec.inatel.br>
2026-04-06 18:58:36 -0300

    Union and Union all
    Learning the union operation, and the differences between union and joins.

--- SQL/Data Camp questions.sql
+++ SQL/Data Camp questions.sql
@@ -169,4 +169,23 @@ FROM populations AS p1
 INNER JOIN populations AS p2
 ON p1.country_code = p2.country_code;
 
--- 
+-- UNION and UNION ALL
+-- Begin your query by selecting all fields from economies2015.
+-- Create a second query that selects all fields from economies2019.
+-- Perform a set operation to combine the two queries you just created, ensuring you do not return duplicates.
+SELECT * 
+FROM economies2015    
+UNION
+SELECT *
+FROM economies2019
+ORDER BY code, year;
+
+-- Perform an appropriate set operation that determines all pairs of country code and year (in that order) from economies and populations, excluding duplicates.
+-- Order by country code and year.
+-- Query that determines all pairs of code and year from economies and populations, without duplicates
+SELECT country_code, year
+FROM populations
+UNION
+SELECT code, year
+FROM economies
+ORDER BY country_code, year;
